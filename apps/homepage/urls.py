d13ebe4d5c58eb66f7a22d612ec60d2b88ef92ce
Barbara <z4beth@gmail.com>
2012-03-30 13:49:32 -0400

    Stupid hack to make the mobile page work. I'll clean this up in ab it

--- apps/homepage/urls.py
+++ apps/homepage/urls.py
@@ -5,5 +5,6 @@ from apps.homepage.views      import *
 
 urlpatterns = [
 
-    (r'/()',             ShowLandingPage) # Must be last
+    (r'/mobile/index.html',      ShowPhonePage), # Must be last
+    (r'/()',             ShowDesktopPage) # Must be last
 ]
--- apps/homepage/views.py
+++ apps/homepage/views.py
@@ -1,19 +1,20 @@
 #!/usr/bin/python
+import logging
 
 from google.appengine.api import memcache
 from google.appengine.ext import webapp
 
 from util.urihandler import URIHandler
 
-class ShowLandingPage(URIHandler):
+class ShowPhonePage(URIHandler):
+    def get(self):
+        template_values = { }
+        
+        self.response.out.write(self.render_page('mobile/index.html', template_values))
+
+class ShowDesktopPage(URIHandler):
     def get(self, page):
         template_values = { }
         
-        html = ''
-        if 'mobile' in page:
-            html = 'mobile/'
-
-        html += 'index.html'
-
-        self.response.out.write(self.render_page(html, template_values))
+        self.response.out.write(self.render_page('index.html', template_values))
 
