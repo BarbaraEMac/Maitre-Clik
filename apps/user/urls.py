90194f671d9eca32f995665214843d2c6f911276
Barbara <z4beth@gmail.com>
2012-04-08 18:18:03 -0400

    Removing User views since we restructed the mobile app

--- apps/user/urls.py
+++ apps/user/urls.py
@@ -1,15 +1,10 @@
 #!/usr/bin/env python
 
 from apps.user.processes import *
-from apps.user.views     import *
+#from apps.user.views     import *
 
 urlpatterns = [
-    # Views
-    (r'/user/newuser',             ShowNewUser),
-    (r'/user/unregistered',        ShowUnregisteredUser),
-
     # Processes
     (r'/user/create',         CreateUser),
     (r'/user/register',       RegisterUser),
-    
 ]
