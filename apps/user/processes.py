ebfe44a892e5418119754c0fcda494615f70b789
Barbara <z4beth@gmail.com>
2012-04-20 14:35:30 -0400

    Dont make a new user if we already have one

--- apps/user/processes.py
+++ apps/user/processes.py
@@ -16,17 +16,20 @@ from util.urihandler        import URIHandler
 
 class CreateUser( URIHandler ):
     def post( self ):
-        first_name  = self.request.get( 'firstName' )
-        last_name   = self.request.get( 'lastName' )
-        email       = self.request.get( 'email' )
-        
-        user        = User.create( first_name, last_name, email )
+        user = self.get_user()
 
-        # Cache the User
-        self.db_user = user
+        if not user:
+            first_name  = self.request.get( 'firstName' )
+            last_name   = self.request.get( 'lastName' )
+            email       = self.request.get( 'email' )
+            
+            user        = User.create( first_name, last_name, email )
 
-        # Have to do this here to avoid a circular reference
-        set_user_cookie( self, user.uuid )
+            # Cache the User
+            self.db_user = user
+
+            # Have to do this here to avoid a circular reference
+            set_user_cookie( self, user.uuid )
         
         self.response.headers['Content-Type'] = "application/json"
         self.response.out.write( json.dumps( { 'uuid' : user.uuid,
